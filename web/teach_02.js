9a3c6f9480a02c0a4e03e5c0f25c8a2cf8b54790
Alec Baird <alec40baird@gmail.com>
2019-04-29 16:57:24 -0600

    works?

--- web/teach_02.js
+++ web/teach_02.js
@@ -2,5 +2,5 @@ function alertUser() {
 	alert("Clicked!");
 }
 function changeColor() {
-	document.getElementById('div2').style.color = 'red';
+	document.getElementById('div2').style.color = document.getElementById('color');
 }
