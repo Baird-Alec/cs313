af7a448b120d4c55a3203824cb8d985ef428f731
Alec Baird <alec40baird@gmail.com>
2019-05-04 20:02:47 -0600

    changed more extensions

--- web/CIT261_links.php
+++ web/CIT261_links.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html>
 <head>
 	<title>CIT 261 Assignments</title>
---
+++ web/Homepage_links.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>CS 313 Assignments</title>
+	<link rel="stylesheet" type="text/css" href="Homepage.css">
+</head>
+<body>
+<h1>CS 313 Assignments</h1>
+<p>This page will hold links to other assignments as they are created.</p><br>
+<a href="Homepage_intro.php">Return to Home</a>
+</body>
+</html>
