6b8eae61d6f5f8e47d43e78307d7683ea1891483
Alec Baird <alec40baird@gmail.com>
2019-05-07 20:42:16 -0600

    passing variable

--- web/Shopping Cart/items.php
+++ web/Shopping Cart/items.php
@@ -7,9 +7,9 @@
 <h1>ALec Baird's Store!</h1>
 
 <h3>Clothing</h3>
-T-Shirt: $25.00<button onclick="add(T-Shirt)"></button><br>
-Jeans: $40.00<button onclick="add(Jeans)"></button><br>
-Sweater: $55.00<button onclick="add(Sweater)"></button><br>
+T-Shirt: $25.00<button onclick="add(\'' + T-Shirt + '\')">Add to Cart</button><br>
+Jeans: $40.00<button onclick="add(\'' + Jeans + '\')">Add to Cart</button><br>
+Sweater: $55.00<button onclick="add(\'' + Sweater + '\')">Add to Cart</button><br>
 </body>
 <script type="text/javascript">
 	function add(item) {
