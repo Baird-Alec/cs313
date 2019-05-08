566b65d6164833ca092ed01d1c7c07ad028004d5
Alec Baird <alec40baird@gmail.com>
2019-05-07 20:49:27 -0600

    Test

--- web/Shopping Cart/items.php
+++ web/Shopping Cart/items.php
@@ -7,13 +7,13 @@
 <h1>ALec Baird's Store!</h1>
 
 <h3>Clothing</h3>
-T-Shirt: $25.00<button onclick="add(\'' + T-Shirt + '\')">Add to Cart</button><br>
-Jeans: $40.00<button onclick="add(\'' + Jeans + '\')">Add to Cart</button><br>
-Sweater: $55.00<button onclick="add(\'' + Sweater + '\')">Add to Cart</button><br>
+T-Shirt: $25.00<button onclick="add(1)">Add to Cart</button><br>
+Jeans: $40.00<button onclick="add(2)">Add to Cart</button><br>
+Sweater: $55.00<button onclick="add(3)">Add to Cart</button><br>
 </body>
 <script type="text/javascript">
-	function add(item) {
-		alert("Test");
+	function add(number) {
+		alert(number);
 	}
 </script>
 </html>
