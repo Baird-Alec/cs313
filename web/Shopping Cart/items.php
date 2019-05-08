9f584968423fa7e0cf2df6f37779945ec61b5c67
Alec Baird <alec40baird@gmail.com>
2019-05-07 20:39:00 -0600

    Items page

--- web/Shopping Cart/items.php
+++ web/Shopping Cart/items.php
@@ -4,6 +4,16 @@
 	<title>Browse Items Page</title>
 </head>
 <body>
+<h1>ALec Baird's Store!</h1>
 
+<h3>Clothing</h3>
+T-Shirt: $25.00<button onclick="add(T-Shirt)"></button><br>
+Jeans: $40.00<button onclick="add(Jeans)"></button><br>
+Sweater: $55.00<button onclick="add(Sweater)"></button><br>
 </body>
+<script type="text/javascript">
+	function add(item) {
+		alert(item);
+	}
+</script>
 </html>
