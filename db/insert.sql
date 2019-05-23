7d3363d202c7425367848d86c51f72f030cd5d55
Alec Baird <alec40baird@gmail.com>
2019-05-22 23:23:04 -0600

    Created insert script

---
+++ db/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO "USER" (USERNAME, FIRST, LAST) VALUES
+('alec40baird', 'Alec',  'Baird'),
+('emmaash', 'Emma', 'Baird'),
+('dkbaird', 'Kathy', 'Baird'),
+('grammy', 'Sheryl', 'Outcelt');
+
+INSERT INTO EVENT (USERNAME, TITLE, START_DATE, END_DATE, START_TIME, END_TIME, LOCATION) VALUES
+('alec40baird', 'Graduation', '07/23/2019', '07/23/2019', '06:00 AM', '05:00 PM', 'Idaho');
