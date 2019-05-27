9445d50df7088e0eb04069040dadc593e61279ed
Alec Baird <alec40baird@gmail.com>
2019-05-26 22:24:12 -0600

    fixed php

--- web/calendar/home.php
+++ web/calendar/home.php
@@ -11,36 +11,36 @@
         Search by Username:<br>
         Search by Location:<br>
         Search All Events:<br>
+        <?php
+        try
+        {
+        $dbUrl = getenv('DATABASE_URL');
+        
+        $dbOpts = parse_url($dbUrl);
+        
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+        
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $ex)
+        {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+        }
+    ?>
+    <?php
+    foreach ($db->query('SELECT USERNAME from "user"') as $row)
+    {
+    echo 'user: ' . $row['username'];
+    echo ' password: ' . $row['password'];
+    echo '<br/>';
+    }
+    ?>
     </body>
 </html>
-<?php
-    try
-    {
-      $dbUrl = getenv('DATABASE_URL');
-    
-      $dbOpts = parse_url($dbUrl);
-    
-      $dbHost = $dbOpts["host"];
-      $dbPort = $dbOpts["port"];
-      $dbUser = $dbOpts["user"];
-      $dbPassword = $dbOpts["pass"];
-      $dbName = ltrim($dbOpts["path"],'/');
-    
-      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    
-      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $ex)
-    {
-      echo 'Error!: ' . $ex->getMessage();
-      die();
-    }
-?>
-<?php
-foreach ($db->query('SELECT USERNAME from "user"') as $row)
-{
-  echo 'user: ' . $row['username'];
-  echo ' password: ' . $row['password'];
-  echo '<br/>';
-}
-?>
