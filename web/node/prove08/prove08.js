dc7cc10c6566a278a4907573d7391d8ef4ea78d8
Alec Baird <alec40baird@gmail.com>
2019-06-18 04:30:01 -0400

    Install Express

--- web/node/prove08/prove08.js
+++ web/node/prove08/prove08.js
@@ -3,14 +3,22 @@ var url = require('url')
 
 http.createServer(function (request, response) {
   var parsed = url.parse(request.url, true)
-  if(parsed.pathname == "home") {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World!');
+  if(parsed.pathname == "/home") {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write("<h1>Hello World!</h1>");
+    response.end();
+  }
+  else if(parsed.pathname == "/getData") {
+    response.writeHead(200, {'Content-Type': 'application/JSON'});
+    var obj = [];
+    obj.push({ "name": "Alec Baird", "class": "cs-313" })
+    var json = JSON.stringify(obj)
+    response.write(json);
     response.end();
   }
   else {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World!');
+    response.writeHead(404, {'Content-Type': 'text/html'});
+    response.write("<h1>404 Error: Not found</h1>");
     response.end();
   }
 }).listen(8888);
