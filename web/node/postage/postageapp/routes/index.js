4980728349f3781c4e2938c08ca6d4320d7246e9
Alec Baird <alec40baird@gmail.com>
2019-06-22 21:44:05 -0600

    Updated index.js

--- web/node/postage/postageapp/routes/index.js
+++ web/node/postage/postageapp/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
-var router = express.Router();
+const app = express();
+const port = process.env.PORT || 3000;
+// tell it to use the public directory as one where static files live
+app.use(express.static(__dirname + '/public'));
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// set up a rule that says requests to "/rates" should be handled by the
+// handleMath function below
+app.get('/rates', handleRates);
+
+// start the server listening
+app.listen(port, function() {
+  console.log('Node app is running on port', port);
 });
-
-module.exports = router;
