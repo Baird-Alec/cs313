a04dee252d26ce46ee64f92aaa75ffa1f29f105c
Alec Baird <alec40baird@gmail.com>
2019-06-22 22:03:27 -0600

    Wrote price logic

--- web/node/postage/postageapp/routes/index.js
+++ web/node/postage/postageapp/routes/index.js
@@ -16,3 +16,108 @@ app.get('/rates', handleRates);
 app.listen(port, function() {
   console.log('Node app is running on port', port);
 });
+
+function handleRates(request, response) {
+  var mailType = request.query.mail;
+  var weight = request.query.weight;
+  createPostage(mailtype, weight);
+}
+
+function createPostage(response, mailtype, weight) {
+
+  var price = 0;
+  if (mailtype == "Letters (Stamped)") {
+    if (weight < 1) {
+      price = 0.55;
+    }
+    else if (weight < 2) {
+      price = 0.70;
+    }
+    else if (weight < 3) {
+      price = 0.85;
+    }
+    else if (weight >= 3) {
+      price = 1.00;
+    }
+  }
+  else if (mailtype== "Letters (Metered)")  {
+    if (weight < 1) {
+      price = 0.50;
+    }
+    else if (weight < 2) {
+      price = 0.65;
+    }
+    else if (weight < 3) {
+      price = 0.80;
+    }
+    else if (weight >= 3) {
+      price = 0.95;
+    }    
+  }
+  else if (mailtype == "Large Envelopes (Flats)") {
+    if (weight < 1) {
+      price = 1.00;
+    }
+    else if (weight < 2) {
+      price = 1.15;
+    }
+    else if (weight < 3) {
+      price = 1.30;
+    }
+    else if (weight < 4) {
+      price = 1.45;
+    }
+    else if (weight < 5) {
+      price = 1.60;
+    }
+    else if (weight < 6) {
+      price = 1.75;
+    }
+    else if (weight < 7) {
+      price = 1.90;
+    }
+    else if (weight < 8) {
+      price = 2.05;
+    }
+    else if (weight < 9) {
+      price = 2.20;
+    }
+    else if (weight < 10) {
+      price = 2.35;
+    }
+    else if (weight < 11) {
+      price = 2.50;
+    }
+    else if (weight < 12) {
+      price = 2.65;
+    }
+    else if (weight >= 12) {
+      price = 2.80;
+    }
+  }
+  else if (mailtype == "First-Class Package Service—Retail") {
+    if (weight <= 4) {
+      price = 3.66;
+    }
+    else if (weight > 4 && weight <= 8) {
+      price = 4.39;
+    }
+    else if (weight > 8 && weight <= 12) {
+      price = 5.19;
+    }
+    else if (weight > 12) {
+      price = 5.71;
+    }
+  }
+  else {
+    price = -1;
+  }
+
+  	// Set up a JSON object of the values we want to pass along to the EJS result page
+	var params = {type: mailtype, price: price};
+
+	// Render the response, using the EJS page "result.ejs" in the pages directory
+	// Makes sure to pass it the parameters we need.
+	response.render('pages/result', params);
+
+}
